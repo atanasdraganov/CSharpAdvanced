f705da4371a28f1d9c71e975871178fcd9b2538d
atanasdraganov <atanasdraganov77@gmail.com>
2021-09-08 01:38:23 +0300

    3rd exercise

---
+++ StackAndQueuesLab/SimpleCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>(Console.ReadLine().Split().Reverse());
+            while(stack.Count>1)
+            {
+                int first = int.Parse(stack.Pop());
+                string operation = stack.Pop();
+                int second = int.Parse(stack.Pop());
+                stack.Push(operation == "+" ? (first + second).ToString() : (first - second).ToString());
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
