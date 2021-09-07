746b92b18888849e1ca154e569be3cc2b00ea86f
atanasdraganov <atanasdraganov77@gmail.com>
2021-09-08 02:23:19 +0300

    5th exercise

---
+++ StackAndQueuesLab/PrintEvenNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> integers = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).Reverse());
+            
+            while(!integers.All(i => i%2==0))
+            {
+                if(integers.Peek()%2==0)
+                {
+                    integers.Enqueue(integers.Dequeue());
+                }
+                else
+                {
+                    integers.Dequeue();
+                }
+            }
+            Console.WriteLine(string.Join(", ", integers.Reverse()));
+        }
+    }
+}
