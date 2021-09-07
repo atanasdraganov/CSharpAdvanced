44640132bc169d3de715ffe5f2b33bc2ddabad6f
atanasdraganov <atanasdraganov77@gmail.com>
2021-09-08 01:48:12 +0300

    4th exercise

---
+++ StackAndQueuesLab/MatchingBrackets/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> indexes = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch(input[i])
+                {
+                    case '(':
+                        indexes.Push(i);
+                        break;
+                    case ')':
+                        int index = indexes.Pop();
+                        Console.WriteLine(input.Substring(index, i-index+1));
+                        break;
+                }
+            }
+        }
+    }
+}
