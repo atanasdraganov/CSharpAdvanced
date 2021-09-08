638467ba04fa66eb793fac91a6df0d291314c00f
atanasdraganov <atanasdraganov77@gmail.com>
2021-09-08 05:41:22 +0300

    7th exercise

---
+++ StackAndQueuesLab/HotPotato/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> children = new Queue<string>(Console.ReadLine().Split());
+            int number = int.Parse(Console.ReadLine());
+
+            while (children.Count != 1)
+            {
+                for (int i = 1; i < number; i++)
+                {
+                    children.Enqueue(children.Dequeue());
+                }
+                Console.WriteLine($"Removed {children.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {children.Dequeue()}");
+        }
+    }
+}
