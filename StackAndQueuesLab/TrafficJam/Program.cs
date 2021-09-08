b5beed868f390f0e62bc62099c49227dfb98271e
atanasdraganov <atanasdraganov77@gmail.com>
2021-09-08 06:10:08 +0300

    8th exercise

---
+++ StackAndQueuesLab/TrafficJam/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            Queue<string> cars = new Queue<string>();
+            int count = 0;
+            string command = Console.ReadLine();
+            while(command!= "end")
+            {
+                if(command == "green")
+                {
+                    for(int i=0; i<N; i++)
+                    {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        count++;
+                    }
+                    command = Console.ReadLine();
+                    continue;
+                }
+                cars.Enqueue(command);
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
