575f50321f8253df81475251fb05c4504a646889
atanasdraganov <atanasdraganov77@gmail.com>
2021-09-08 05:31:23 +0300

    6th exercise

---
+++ StackAndQueuesLab/Supermarket/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> customers = new Queue<string>();
+            string input = Console.ReadLine();
+            while(input != "End")
+            {   
+
+                if(input == "Paid")
+                {
+                    foreach (var customer in customers)
+                    {
+                        Console.WriteLine(customer);
+                    }
+                    customers.Clear();
+                    input = Console.ReadLine();
+                    continue;
+                }
+                
+                customers.Enqueue(input);
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{customers.Count} people remaining.");
+        }
+    }
+}
