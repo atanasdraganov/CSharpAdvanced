6d8ac277d70adae5f3d825c8a0b61ade606423ac
atanasdraganov <atanasdraganov77@gmail.com>
2021-09-13 12:25:52 +0300

    7th exercise

---
+++ StackAndQueuesExercise/BasicStackOperations/TruckTour/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfPetrolPumps = int.Parse(Console.ReadLine());
+            Queue<int[]> petrolPumps = new Queue<int[]>();
+            for (int i = 0; i < countOfPetrolPumps; i++)
+            {
+                int[] petrolPump = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                petrolPumps.Enqueue(petrolPump);
+            }
+            int index = 0;
+            while (true)
+            {
+                int totalFuel = 0;
+                foreach (int[] petrolPump in petrolPumps)
+                {
+                    int petrolAmount = petrolPump[0];
+                    int distance = petrolPump[1];
+
+                    totalFuel += petrolAmount - distance;
+                    if (totalFuel < 0)
+                    {
+                        petrolPumps.Enqueue(petrolPumps.Dequeue());
+                        index++;
+                        break;
+                    }
+                }
+                if(totalFuel>= 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(index);
+        }
+    }
+}
