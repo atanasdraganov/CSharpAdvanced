79a41e2558514ab131ec79f5ba546f169b29f30d
atanasdraganov <atanasdraganov77@gmail.com>
2021-09-13 11:01:52 +0300

    4th exercise

---
+++ StackAndQueuesExercise/BasicStackOperations/Fast Food/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int food = int.Parse(Console.ReadLine());
+            int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(orders);
+            int sum = 0;
+            Console.WriteLine(orders.Max());
+            while (queue.Count > 0)
+            {
+                int firstInLine = queue.Peek();
+                sum += firstInLine;
+                if (sum <= food)
+                {
+                    queue.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+                    break;
+                }
+            }
+            if(queue.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            
+        }
+    }
+}
