f3fdf47b1d19142cda30179ac0e739a94bf23065
atanasdraganov <atanasdraganov77@gmail.com>
2021-09-15 22:35:48 +0300

    3rd exercise

---
+++ MultidimensionalArraysLab/SumMatrixElements/PrimaryDiagonal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+            int sum = 0;
+            for (int row = 0; row < size; row++)
+            {
+                int[] colElements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = colElements[col];
+                    if(row == col)
+                    {
+                        sum += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
