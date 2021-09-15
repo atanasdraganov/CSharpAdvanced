2d2f4a199627c49c23d4d42eacb1b1abe5ba4741
atanasdraganov <atanasdraganov77@gmail.com>
2021-09-15 17:19:52 +0300

    5th exercise

---
+++ StackAndQueuesExercise/BasicStackOperations/FashionBoutique/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothesValues = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int currentCapacity = 0;
+            int rackCount = 1;
+            
+            while(clothesValues.Count>0)
+            {
+                if(currentCapacity + clothesValues.Peek() > rackCapacity)
+                {
+                    currentCapacity = 0;
+                    rackCount++;
+                }
+                currentCapacity += clothesValues.Pop();
+            }
+            Console.WriteLine(rackCount);
+        }
+    }
+}
