9f68b845efaac807c3f704c61486c5987975d985
atanasdraganov <atanasdraganov77@gmail.com>
2021-09-15 21:34:07 +0300

    9th exercise

---
+++ StackAndQueuesExercise/BasicStackOperations/SimpleTextEditor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nrOfOperations = int.Parse(Console.ReadLine());
+            Stack<string> stack = new Stack<string>();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < nrOfOperations; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+                if (command[0] == "1")
+                {
+                    stack.Push(sb.ToString());
+                    sb.Append(command[1]);
+                }
+                else if(command[0] == "2")
+                {
+                    stack.Push(sb.ToString());
+                    int countOfElements = int.Parse(command[1]);
+                    sb.Remove(sb.Length-countOfElements, countOfElements);
+                }
+                else if(command[0] == "3")
+                {
+                    Console.WriteLine(sb[int.Parse(command[1])-1]);
+                } 
+                else if(command[0] == "4")
+                {
+                    if(sb.Length == 0)
+                    {
+                        sb.Append(stack.Pop());
+                    }
+                    else
+                    {
+                        sb.Replace(sb.ToString(), stack.Pop());
+                    }
+                }
+            }
+        }
+    }
+}
