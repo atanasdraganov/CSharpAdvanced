c70f36a43caca03a0b8e6c4cd1f4f3737ddc2139
atanasdraganov <atanasdraganov77@gmail.com>
2021-09-16 14:26:11 +0300

    1st exercise

---
+++ MultidimensionalArraysExercise/MultidimensinalArraysExercise/MultidimensinalArraysExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+            int sumPrimary = 0;
+            int sumSecondary = 0;
+            for (int row = 0; row < size; row++)
+            {
+                int[] colElements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = colElements[col];
+                    if (row == col)
+                    {
+                        sumPrimary += matrix[row, col];
+                    }
+                    if(row+col == size-1)
+                    {
+                        sumSecondary += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(Math.Abs(sumPrimary-sumSecondary));
+        }
+    }
+}
